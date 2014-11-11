6a74842424edc7cab7158b25bb855267af5ce036
ikona23@gmail.com <ikona23@gmail.com>
2014-11-12 04:32:57 +1300

    first commit4

--- config/application.rb
+++ config/application.rb
@@ -21,11 +21,11 @@ module Depot
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
+    config.assets.initialize_on_precompile = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
+    config.assets.enabled = true
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
